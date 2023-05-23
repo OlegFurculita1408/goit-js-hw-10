eb9bfaf9e46dd8193e576eff411af12023f17a4d
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-05-23 21:28:43 +0300

    add files js

---
+++ src/cat-api.js
@@ -0,0 +1,6 @@
+function fetchBreeds() {
+    
+};
+
+
+export {fetchBreeds};
---
+++ src/index.js
@@ -0,0 +1,5 @@
+// 'api_key=live_2nUbRAaTiUgWYWS8KXuGfgwBOF83b1r9dJuyQIO8RTidjVuc65yHuEVxepuqSxWH'
+
+// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
+
+import { fetchBreeds } from './cat-api.js';
