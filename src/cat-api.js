d5efe274764b49d8a89c5efcd59402e6cb15810b
Oleg Furculita <Oleg.Furculita1408@gmail.com>
2023-06-02 20:30:44 +0300

    import function

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,47 +1,27 @@
 
 const BASE_URL = 'https://api.thecatapi.com/v1/';
 const END_POINT = 'breeds';
-const END_POINT_IMG =  'images/search';
+const END_POINT_IMG = 'images/';
 const API_KEY = 'live_2nUbRAaTiUgWYWS8KXuGfgwBOF83b1r9dJuyQIO8RTidjVuc65yHuEVxepuqSxWH';
 
-const select = document.querySelector('.breed-select');
-const div = document.querySelector('.cat-info');
-const loader = document.querySelector('.loader');
-const error = document.querySelector('.error');
 
 
-function fetchBreeds() {
+export function fetchBreeds() {
     return fetch(`${BASE_URL}${END_POINT}?api_key=${API_KEY}`).then(resp => {
         if(!resp.ok){
             throw new Error(resp.statusText);
+            
         }
 
         return resp.json();
         }
     );
 };
-fetchBreeds()
-    .then((data) => select.insertAdjacentHTML('beforeend', createOptionSelect(data)))
-    .catch((err) => console.log(err))
 
-function createOptionSelect(arr) {
-    return arr.map(({name, reference_image_id}) => `
-        <option value="${reference_image_id}">${name}</option>`).join('');
-}
 // ----------------------------------------------------------------------------------
 
-select.addEventListener('change', handlerSearch);
-
-    function handlerSearch(evt) {
-        evt.preventDefault();
-        const { value } = evt.currentTarget;
-        fetchCatByBreed(value)
-            .then((data) => div.innerHTML = createMarkup(data))
-            .catch((err) => console.log(err))
-    }
-
-function fetchCatByBreed(breendId) {
-    return fetch(`${BASE_URL}images/${breendId}`).then(resp => {
+export function fetchCatByBreed(breendId) {
+    return fetch(`${BASE_URL}${END_POINT_IMG}${breendId}`).then(resp => {
         if(!resp.ok){
             throw new Error(resp.statusText)
         }
@@ -50,13 +30,3 @@ function fetchCatByBreed(breendId) {
         }
     );
 };
-    
-function createMarkup(obj) {
-    const { name, origin, temperament, description } = obj.breeds[0]
-    return `
-        <img src="${obj.url}" alt="cat" width="400px">
-        <h2 class="name">${name}</h2>
-        <h3>${origin}</h3>
-        <h3 class="origin">${temperament}</h3>
-        <p class="description">${description}</p>`;
-}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,42 @@
-// 'api_key=live_2nUbRAaTiUgWYWS8KXuGfgwBOF83b1r9dJuyQIO8RTidjVuc65yHuEVxepuqSxWH'
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
-// https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=beng&api_key=REPLACE_ME
 
-// import { fetchBreeds } from './cat-api.js';
+const select = document.querySelector('.breed-select');
+const div = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
+
+
+fetchBreeds()
+    .then((data) => select.insertAdjacentHTML('beforeend', createOptionSelect(data)))
+    .catch((err) => console.log(err))
+
+function createOptionSelect(arr) {
+    return arr.map(({name, reference_image_id}) => `
+        <option value="${reference_image_id}">${name}</option>`).join('');
+}
+
+select.addEventListener('change', handlerSearch);
+
+    function handlerSearch(evt) {
+        evt.preventDefault();
+        const { value } = evt.currentTarget;
+        fetchCatByBreed(value)
+            .then((data) => div.innerHTML = createMarkup(data))
+            .catch((err) => console.log(err))
+        if (value === 0 ){
+            error.classList.remove("error")
+        }
+    }
+        
+function createMarkup(obj) {
+    const { name, origin, temperament, description } = obj.breeds[0]
+    return `
+        <img src="${obj.url}" alt="cat" width="400px">
+        <div class='container'>
+        <h2 class="name">Name, ${name}.</h2>
+        <h3>Country of origin:${origin}</h3>
+        <h3 class="origin">Temperament: ${temperament}</h3>
+        <p class="description">Description: ${description}</p>
+        </div>`;
+}
